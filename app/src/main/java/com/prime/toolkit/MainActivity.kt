0b5ebfce161f1cc92e6a50655bc9922ced910b47
Zakir Sheikh <sheikhzakirahmad@gmail.com>
2023-06-19 11:49:31 +0530

    * Added Material3 extension methods. * Fixed the position of footer in ListTile.kt

--- app/src/main/java/com/prime/toolkit/MainActivity.kt
+++ app/src/main/java/com/prime/toolkit/MainActivity.kt
@@ -3,7 +3,14 @@ package com.prime.toolkit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,15 +18,23 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.prime.toolkit.ui.theme.ToolkitTheme
+import com.primex.core.padding
+import com.primex.core.rememberVectorPainter
 import com.primex.material2.Label
+import com.primex.material2.ListTile
+import com.primex.material2.OutlinedButton
 import com.primex.material2.Placeholder
 import com.primex.material2.Preference
 import com.primex.material2.SwitchPreference
+import com.primex.material3.IconButton
 import com.primex.preferences.Preferences
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +61,34 @@ fun Greeting(name: String) {
 @Preview
 @Composable
 fun PLaceholder() {
-    Surface(modifier = Modifier) {
-      SwitchPreference(title = "Dark Mode", checked = false, onCheckedChange = {}, summery = "Toggle dark Mode On/Off", icon = Icons.Outlined.Star)
+    Surface {
+        com.primex.material3.ListTile(
+            headline = { Text(text = "Headline") },
+            leading = {
+                Icon(imageVector = Icons.Outlined.Star, contentDescription = null)
+            },
+            trailing = {
+                Icon(
+                    imageVector = Icons.Outlined.MoreVert,
+                    contentDescription = null
+                )
+            },
+            subtitle = { Text(text = "This is the subtitle text and this maybe represetnts the desc text of headline.") },
+            overline = { Text(text = "Overline") },
+            footer = {
+                Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+                    repeat(10) {
+                        OutlinedButton(
+                            label = "index: $it",
+                            onClick = { /*TODO*/ },
+                            leading = rememberVectorPainter(
+                                image = Icons.Outlined.Star
+                            ),
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
+                }
+            }
+        )
     }
 }
---
+++ material3/src/main/java/com/primex/material3/Delegates.kt
@@ -0,0 +1,272 @@
+package com.primex.material3
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.RenderVectorGroup
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.PlatformParagraphStyle
+import androidx.compose.ui.text.PlatformSpanStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontSynthesis
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.LocaleList
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.text.style.TextGeometricTransform
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.primex.core.Text
+
+private const val TAG = "Delegates"
+
+/**
+ * A simple `[IconButton] composable that takes [painter] as content instead of content composable.
+ * @see IconButton
+ */
+@Composable
+inline fun IconButton(
+    icon: Painter,
+    contentDescription: String?,
+    noinline onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    IconButton(
+        onClick = onClick,
+        modifier,
+        enabled,
+        colors,
+        interactionSource,
+        content = {
+            Icon(painter = icon, contentDescription = contentDescription)
+        }
+    )
+}
+
+/**
+ * @see IconButton
+ */
+@Composable
+inline fun IconButton(
+    icon: ImageVector,
+    contentDescription: String?,
+    noinline onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    IconButton(
+        rememberVectorPainter(image = icon),
+        contentDescription,
+        onClick,
+        modifier,
+        enabled,
+        colors,
+        interactionSource
+    )
+}
+
+/**
+ * ### Advanced Text Composable
+ * An enhanced version of the original `Text` composable that allows direct input of a
+ * `CharSequence`, reducing the need for separate composable. This advanced version eliminates the
+ * requirement to convert the text to a `String` or `AnnotatedString`.
+ *
+ * @see androidx.compose.material3.Text
+ * @throws IllegalStateException If [text] is neither a [String] nor an [AnnotatedString].
+ */
+@Composable
+inline fun Text(
+    text: CharSequence,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+    noinline onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current
+) {
+    when(text){
+        is String -> Text(text, modifier, color, fontSize, fontStyle, fontWeight, fontFamily, letterSpacing, textDecoration, textAlign, lineHeight, overflow, softWrap, maxLines, minLines, onTextLayout, style)
+        is AnnotatedString -> Text(text, modifier, color, fontSize, fontStyle, fontWeight, fontFamily, letterSpacing, textDecoration, textAlign, lineHeight, overflow, softWrap, maxLines, minLines, inlineContent, onTextLayout, style)
+        else -> error("$text must be either AnnotatedString or String!!")
+    }
+}
+
+/**
+ * Creates a button composable with the specified properties
+ * @param label The label text to be displayed on the button. Can be a [String], [CharSequence], or [AnnotatedString].
+ * @param icon The optional icon to be displayed on the button. Accepts a [Painter] representing a vector or image asset.
+ * @see androidx.compose.material3.Button
+ */
+@Composable
+inline fun Button(
+    label: CharSequence,
+    noinline onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    icon: Painter? = null,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.shape,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    androidx.compose.material3.Button(
+        onClick,
+        modifier,
+        enabled,
+        shape,
+        colors,
+        elevation,
+        border,
+        contentPadding,
+        interactionSource,
+        content = {
+            if (icon != null) Icon(
+                painter = icon,
+                contentDescription = null,
+                modifier = Modifier.padding(end = ButtonDefaults.IconSpacing)
+            )
+            Text(text = label)
+        }
+    )
+}
+
+@Composable
+inline fun OutlinedButton(
+    label: CharSequence,
+    noinline onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    icon: Painter? = null,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.outlinedShape,
+    colors: ButtonColors = ButtonDefaults.outlinedButtonColors(),
+    elevation: ButtonElevation? = null,
+    border: BorderStroke? = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface.copy(0.12f)),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    androidx.compose.material3.OutlinedButton(
+        onClick,
+        modifier,
+        enabled,
+        shape,
+        colors,
+        elevation,
+        border,
+        contentPadding,
+        interactionSource
+    ) {
+        if (icon != null)
+            Icon(painter = icon, contentDescription = null)
+        Text(
+            text = label,
+            Modifier.padding(start = ButtonDefaults.IconSpacing)
+        )
+    }
+}
+
+@Composable
+inline fun TextButton(
+    label: CharSequence,
+    noinline onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    icon: Painter? = null,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.textShape,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+    elevation: ButtonElevation? = null,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    androidx.compose.material3.TextButton(
+        onClick,
+        modifier,
+        enabled,
+        shape,
+        colors,
+        elevation,
+        border,
+        contentPadding,
+        interactionSource
+    ) {
+        if (icon != null)
+            Icon(painter = icon, contentDescription = null)
+        Text(
+            text = label,
+            Modifier.padding(start = ButtonDefaults.IconSpacing)
+        )
+    }
+}
+
+
+/**
+ * Returns a Composable function if the condition is true, otherwise returns null.
+ *
+ * @param condition The boolean condition that determines if the composable function should be returned.
+ * @param content The composable function to be returned if the condition is true.
+ * @return The composable function if the condition is true, otherwise null.
+ */
+internal inline fun composableOrNull(condition: Boolean, noinline content: @Composable () -> Unit) = when (condition) {
+    true -> content
+    else -> null
+}
