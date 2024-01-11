2d4d8f429c97879e7851072a904f2b92ae5117c1
Zakir Sheikh <sheikhzakirahmad@live.com>
2024-01-11 10:18:19 +0530

    Updated compose compiler to version 1.5.8  and compose to 1.6.0-rc01

---
+++ core-ktx/src/main/java/com/primex/core/ExperimentalToolkitApi.kt
@@ -0,0 +1,9 @@
+package com.primex.core
+
+
+@RequiresOptIn(
+    "This material API is experimental and is likely to change or to be removed in" +
+            " the future."
+)
+@Retention(AnnotationRetention.BINARY)
+annotation class ExperimentalMaterialApi
